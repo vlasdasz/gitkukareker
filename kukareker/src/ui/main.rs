bdf5181c19ba2176e01b399196e63718535b22f0
Vladas Zakrevskis <146100@gmail.com>
2025-03-19 01:53:25 +0200

    check status with git2

--- kukareker/src/ui/main.rs
+++ kukareker/src/ui/main.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+
+use git2::{Repository, StatusOptions};
 use rfd::FileDialog;
 use test_engine::{
     refs::Weak,
@@ -14,7 +17,7 @@ use crate::model::State;
 #[view]
 pub struct Main {
     #[init]
-    repo_name: DropDown,
+    repo_name: DropDown<PathBuf>,
     open:      Button,
 }
 
@@ -23,6 +26,11 @@ impl Setup for Main {
         Window::set_title("GitKukareker");
 
         self.repo_name.place().tl(20).size(400, 100);
+        self.repo_name
+            .custom_format(|path| path.file_name().unwrap().to_string_lossy().to_string());
+        self.repo_name.on_changed(move |path| {
+            self.repo_selected(&path);
+        });
 
         self.open.set_text("Open");
         self.open
@@ -39,8 +47,7 @@ impl Setup for Main {
 
 impl Main {
     fn update(mut self: Weak<Self>) {
-        self.repo_name
-            .set_values(State::repos().map(|repo| repo.file_name().unwrap().to_string_lossy().to_string()));
+        self.repo_name.set_values(State::repos().collect());
     }
 
     fn on_open(self: Weak<Self>) {
@@ -50,5 +57,22 @@ impl Main {
 
         State::add_repo(result);
         self.update();
+    }
+
+    #[allow(clippy::unused_self)]
+    fn repo_selected(self: Weak<Self>, path: &PathBuf) {
+        let Ok(repo) = Repository::discover(path) else {
+            panic!("no repo")
+        };
+
+        let mut status_opts = StatusOptions::new();
+        status_opts.include_untracked(true);
+
+        let statuses = repo.statuses(Some(&mut status_opts)).unwrap();
+
+        for status in &statuses {
+            dbg!(&status.status());
+            dbg!(&status.path());
+        }
     }
 }
